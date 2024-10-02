c69fc3c18cc3c54dbd0dd5a56aed1f0a1d6eb7a7
Bruno Martins <jbrunno17@gmail.com>
2024-10-02 20:04:16 -0300

    Finish iteration 1 with tightly coupled code

---
+++ src/main/java/com/in28minutes/spring/learn_spring_framework/game/MarioGame.java
@@ -0,0 +1,23 @@
+package com.in28minutes.spring.learn_spring_framework.game;
+
+public class MarioGame {
+    public MarioGame() {
+        System.out.println("MarioGame Created");
+    }
+
+    public void up(){
+        System.out.println("Jump");
+    }
+
+    public void down(){
+        System.out.println("Go into a hole");
+    }
+
+    public void left(){
+        System.out.println("Go left");
+    }
+
+    public void right(){
+        System.out.println("Go right");
+    }
+}
